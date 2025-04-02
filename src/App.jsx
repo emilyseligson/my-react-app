50815608d9fea1ed1c7891cc9dceb3c60f95849c
Emily Seligson <e.d.seligson@wustl.edu>
2025-04-02 13:41:36 -0500

    square displays x on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-function Square({value}) {
-  return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue('X');
+    console.log('clicked!');
+  }
+  return <button className="square" onClick={handleClick}
+  >{value}</button>;
+
 }
 
 export default function Board() {
